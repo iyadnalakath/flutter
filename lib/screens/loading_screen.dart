7d3666902c894279685d22ace2f712b29d4e4a94
iyad nalakath <120023177+iyadnalakath@users.noreply.github.com>
2023-07-17 13:02:11 +0530

    automating detecting locationn added witout clicking get location button

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -8,14 +8,63 @@ class LoadingScreen extends StatefulWidget {
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
+// class _LoadingScreenState extends State<LoadingScreen> {
+//
+//   // void getLocation() async {
+//   //
+//   //   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+//   //   print(position);
+//   //
+//   // }
+//
+//   void getLocation() async {
+//     // Request permission to access the device's location
+//     PermissionStatus permissionStatus = await Permission.location.request();
+//
+//     if (permissionStatus.isGranted) {
+//       // Permission granted, get the current location
+//       Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+//       print(position);
+//     } else {
+//       // Permission denied, handle accordingly
+//       print('Permission denied');
+//     }
+//   }
+//
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {
+//             getLocation();
+//             // Get the current location
+//           },
+//           child: Text('Get Location'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+
+
+
+
+
+
+// here i want when enter to this page it want print the location without clicking get location. so there is no need of get location button.so is there any way?
+// yes we can below are the steps
+
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  // void getLocation() async {
-  //
-  //   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-  //   print(position);
-  //
-  // }
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
 
   void getLocation() async {
     // Request permission to access the device's location
@@ -31,19 +80,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-            // Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+        child: Text('Getting Location...'),
       ),
     );
   }
